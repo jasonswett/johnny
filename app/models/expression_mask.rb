40b57e8975e72fab49d5f3fef3fe143ebeecd722
Jason Swett <jason@benfranklinlabs.com>
2024-07-06 12:39:05 -0400

    Add generation ability.

--- app/models/expression_mask.rb
+++ app/models/expression_mask.rb
@@ -1,4 +1,8 @@
 class ExpressionMask
+  VALID_MASKS = [
+    "article adjective noun verb noun period"
+  ]
+
   def initialize(text)
     @parts_of_speech = text.split
   end
@@ -7,5 +11,9 @@ class ExpressionMask
     @parts_of_speech.map { |part_of_speech| Token.part_of_speech(part_of_speech).sample }
       .map { |token| token ? token.value : "?" }
       .join(" ")
+  end
+
+  def self.generate_sentence
+    new(VALID_MASKS.sample).evaluate
   end
 end
